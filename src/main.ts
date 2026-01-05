cfddbb264f0c0d5d09752f75e79adca65ef36d96
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2026-01-05 15:37:26 +0330

    fix bug as it was bad port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 
+
 import { AppModule } from './app.module.js';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
 }
 
 bootstrap();
